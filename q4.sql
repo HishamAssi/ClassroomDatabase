f8184d1324b9889ad03074453adbe67ceea7b35a
Hisham El-Assi <hisham96assi@gmail.com>
2017-12-02 21:22:43 -0500

    First draft, leggo.

---
+++ q4.sql
@@ -0,0 +1,20 @@
+CREATE VIEW questions AS
+SELECT includes.questionId, questionType FROM includes 
+JOIN question ON includes.questionId=question.questionId
+WHERE quizid='Pr1-220310';
+
+CREATE VIEW students AS
+SELECT s_Id FROM took JOIN class ON took.c_id=class.c_id
+WHERE class.grade='grade 8' AND class.room='room 120' AND class.teacher='Mr Higgins';
+
+CREATE VIEW studentResponses AS
+SELECT questionId, students.s_id, answer, questionType FROM students
+JOIN studentResponse ON students.s_id=studentResponse.s_id
+WHERE quizid='Pr1-220310' AND answer IS NULL OR answer='no response given';
+
+CREATE VIEW allInfo AS
+SELECT s_id, studentResponses.questionId, question.questionText
+FROM studentResponses JOIN question 
+ON studentResponses.questionId=question.questionId;
+
+SELECT * FROM allInfo;
