6e1bc04e91aff71a625c5b34d02a0aa1276939bc
Hisham El-Assi <hisham.el.assi@mail.utoronto.ca>
2017-12-04 14:31:34 -0500

    Added comments and constraints

--- schema.sql
+++ schema.sql
@@ -2,19 +2,30 @@ drop schema if exists quizschema cascade;
 create schema quizschema;
 set search_path to quizschema;
 
+-- TODO: create our own FD? If not, how can we have redundancy?
+-- TODO: query vs select for q1-q5.
+-- TODO: MCQ having at least 2 options.
+
+
 CREATE TABLE student(
-    s_id BIGINT PRIMARY KEY,
+    s_id varchar(10) PRIMARY KEY CHECK char_length(s_id) = 10,
     firstname varchar(255),
     lastname varchar(255) NOT NULL
 );
 
 
 CREATE TABLE class(
-    c_id INT PRIMARY KEY,
+    c_id SERIAL PRIMARY KEY,
     room varchar(255) NOT NULL,
     grade varchar(255) NOT NULL,
     teacher varchar(255) NOT NULL,
+    -- There is a tradeoff between adding a constraint
+    -- on maximum one teacher per room and the number
+    -- of grades allowed in a room.
+    -- It is also not possible to constrain the number
+    -- of grades in a room.
     UNIQUE(room, grade, teacher)
+    
 );
 
 CREATE TABLE took(
@@ -26,27 +37,37 @@ CREATE TABLE took(
 
 CREATE TABLE quiz(
     quizid TEXT PRIMARY KEY,
+    c_Id INT REFERENCES took(c_id),
     title VARCHAR(255),
     q_timestamp TIMESTAMP NOT NULL,
     isHint BOOLEAN NOT NULL
 );
 
+CREATE TYPE question_type AS ENUM(
+	'MCQ', 'Numeric', 'TF');
+
 CREATE TABLE question(
     questionId INT PRIMARY KEY,
     questionText TEXT NOT NULL,
-    questionType TEXT NOT NULL
+    questionType question_type NOT NULL
 );
 
 CREATE TABLE includes(
+    -- We cannot enforce at least one question per quiz because
+    -- quizId is a subset of the quiz(quizId) so it might
+    -- be that quiz does not include any questions if it
+    -- does not exist in this table.
     quizid TEXT REFERENCES quiz(quizid),
     questionid INT REFERENCES question(questionid),
     q_weight INT NOT NULL,
-    PRIMARY KEY(quizid, questionid) 
+    PRIMARY KEY(quizid, questionid)
 );
 
 
 CREATE TABLE MultipleChoice(
     questionId INT REFERENCES question(questionId),
+    -- Checking that there are at least 2 options is
+    -- not doable without a subquery. TODO: ASK OH.
     answerOption TEXT NOT NULL,
     isAnswer BOOLEAN NOT NULL,
     hint TEXT,
@@ -59,7 +80,9 @@ CREATE TABLE NumericQuestions(
     endRange INT,
     -- If isAnswer is true then startRange and endRange must be the same.
     isAnswer BOOLEAN NOT NULL,
-    hint TEXT,
+    CHECK startRange<=endRange,
+    CHECK isAnswer=TRUE AND startRange=endRange,
+    hint VARCHAR(255),
     PRIMARY KEY(questionId, startRange, endRange)
 );
 
@@ -71,11 +94,13 @@ CREATE TABLE true_false(
 CREATE TABLE studentResponse(
     -- TODO: constrain student to only take quiz in a class they are in. 
     questionId INT REFERENCES question(questionId),
-    quizid TEXT REFERENCES quiz(quizid),
+    quizid VARCHAR(255) REFERENCES quiz(quizid),
     s_id BIGINT REFERENCES student(s_id),
-    -- TODO: answer for mcq lies within options.
-    answer TEXT,
-    questionType TEXT NOT NULL,
+    -- Since we combined the answers of all question types in one table,
+    -- we cannot check that the mcq answer exists as an option in the
+    -- MultipleChoice table.
+    answer VARCHAR(255),
+    questionType question_type NOT NULL,
     PRIMARY KEY(questionId, quizId, s_id)
 );
 
