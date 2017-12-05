e8c5b84f3555dcaf102a2e48876ebe7ecee0c42e
Hisham El-Assi <hisham96assi@gmail.com>
2017-12-05 05:29:31 -0500

    Just fixing stuff :)

--- q1.sql
+++ q1.sql
@@ -1,15 +1,7 @@
 
-set search_path to quizschema;
-drop table if exists q1 cascade;
+SET search_path TO quizschema;
 
-CREATE TABLE q1(
-	fullname varchar(510),
-	s_id BIGINT PRIMARY KEY
-);
-
-insert into q1 (
-	SELECT firstname || ' ' || lastname as fullname, s_id
-	FROM student
-);	
+SELECT firstname || ' ' || lastname as fullname, s_id
+FROM student;	
 
 
