52b1c39e28f5643a31d4a1f329c73c97aa83bf68
El Sanyoura Lana <elsanyo1@teach.cs.toronto.edu>
2017-12-06 17:54:34 -0500

    Revamped q2 and q3

--- q2.sql
+++ q2.sql
@@ -6,8 +6,8 @@ DROP VIEW IF EXISTS trueFalseHintCount CASCADE;
 DROP VIEW IF EXISTS MCQHintCount CASCADE;
 DROP VIEW IF EXISTS allCounts CASCADE;
 DROP VIEW IF EXISTS countsWithText CASCADE;
-
-
+DROP VIEW IF EXISTS ZeroMCQ CASCADE;
+DROP VIEW IF EXISTS ZeroNum CASCADE;
 
 CREATE VIEW NumericHintCount AS
 	SELECT questionId, count(hint) as hints
@@ -15,32 +15,35 @@ CREATE VIEW NumericHintCount AS
 	GROUP BY questionId;
 
 CREATE VIEW trueFalseHintCount AS
-	SELECT questionId, 0 as hints
+	SELECT questionId, NULL  as hints
 	FROM true_false;
 
 CREATE VIEW MCQHintCount AS
 	SELECT questionId, count(hint) as hints
 	FROM MultipleChoiceHints
 	GROUP BY questionId;
-CREATE VIEW ZeroHints AS
-	SELECT questionId, 0 as hints
-	FROM (SELECT * FROM 
-		(SELECT * 
-		 FROM (SELECT questionId FROM MultipleChoice) 
+
+CREATE VIEW ZeroMCQ AS
+	SELECT questionId, 0 as hints 
+ 	FROM ((SELECT questionId FROM MultipleChoice) 
 		       except 
-		      (SELECT questionId FROM MultipleChoiceHints))
-		 UNION
-		(SELECT * 
-		 FROM (SELECT questionId FROM NumericQuestions) 
+	(SELECT questionId FROM MultipleChoiceHints)  ) AS MCQ;
+	
+CREATE VIEW ZeroNum AS
+	SELECT questionId, 0 as hints 
+ 	FROM ((SELECT questionId FROM NumericQuestions) 
 		       except 
-		      (SELECT questionId FROM NumericQuestionsHints))) AS ZEROHINT 
+	(SELECT questionId FROM NumericQuestionsHints ) ) AS NUM;
+	
+
 
 CREATE VIEW allCounts as 
 	SELECT * 
 	FROM ((SELECT * FROM  NumericHintCount) UNION 
 	(SELECT * FROM  trueFalseHintCount) UNION 
 	(SELECT * FROM MCQHintCount) UNION
-	(SELECT * FROM ZeroHints))
+	(SELECT * FROM ZeroMCQ) UNION
+	(SELECT * FROM ZeroNum  ))
 	 AS allQuestions;
 
 CREATE VIEW countsWithText AS
